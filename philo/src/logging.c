8076fa755a0b67caf0477bd56493d5f721f5f220
Niklas Burchhardt <niklas@burchhardt.de>
2026-02-17 14:20:42 +0100

    bonus done

--- philo/src/logging.c
+++ philo/src/logging.c
@@ -6,7 +6,7 @@
 /*   By: nburchha <nburchha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 01:48:14 by niklasburch       #+#    #+#             */
-/*   Updated: 2024/06/01 14:00:50 by nburchha         ###   ########.fr       */
+/*   Updated: 2025/11/19 15:26:04 by nburchha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	print_status(t_philo *philo, char *status)
 {
 	pthread_mutex_lock(&philo->data->print_mutex);
-	printf("%llu %d %s\n", get_time() - philo->data->start,
+	printf("%lu %d %s\n", get_time() - philo->data->start,
 		philo->id, status);
 	pthread_mutex_unlock(&philo->data->print_mutex);
 }
